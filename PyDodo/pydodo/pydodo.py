7a775b475ece4393cb029f9137d44a40f35db158
Ruairidh MacLeod <5160559+rkm@users.noreply.github.com>
2019-02-06 15:00:59 +0000

    Add BlueBird ping test

--- PyDodo/pydodo/pydodo.py
+++ PyDodo/pydodo/pydodo.py
@@ -1,4 +1,5 @@
 from .create_aircraft import create_aircraft
+from .utils import ping_bluebird
 
 
 class PyDodo:
@@ -8,3 +9,7 @@ class PyDodo:
 
 	def create_test(self):
 		create_aircraft('whee')
+
+	def ping_test(self):
+		resp = ping_bluebird()
+		print('Ping: {}'.format('pong!' if resp else 'nope!'))
--- PyDodo/pydodo/utils.py
+++ PyDodo/pydodo/utils.py
@@ -1,3 +1,5 @@
+import requests
+
 from . import settings
 
 
@@ -7,3 +9,24 @@ def construct_endpoint_url(endpoint):
 
 def get_bluebird_url():
 	return 'http://{}:{}'.format(settings.BB_HOST, settings.BB_PORT)
+
+
+def ping_bluebird():
+	endpoint = "pos"
+
+	url = construct_endpoint_url(endpoint)
+	print('ping bluebird on {}'.format(url))
+
+	# /pos endpoint only supports GET requests, this should return an error if BlueBird is running
+	# on the specified host
+
+	try:
+		resp = requests.post(url)
+	except requests.exceptions.ConnectionError as e:
+		print(e)
+		return False
+
+	if resp.status_code == 405:
+		return True
+
+	return False
