11305caadee9b898945dc151db595520ea1ac35d
radka-j <r.jersakova@gmail.com>
2019-02-08 10:15:45 +0000

    fix return value, add docstring

--- PyDodo/pydodo/reset_simulation.py
+++ PyDodo/pydodo/reset_simulation.py
@@ -3,11 +3,18 @@ import requests
 from .utils import construct_endpoint_url
 
 def reset_simulation():
+    """
+    Reset the simulation.
+
+    :return : boolean, TRUE indicates success
+    """
     endpoint = "ic"
     url = construct_endpoint_url(endpoint)
     resp = requests.post(url)
 
+    # code 418 is temporary
     if resp.status_code == 418:
         return True
     if resp.status_code == 202:
         return True
+    return False
