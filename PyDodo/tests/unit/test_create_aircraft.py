8ec6a06945bffbd1f18da5989f63862e504ddb7a
radka-j <r.jersakova@gmail.com>
2019-02-14 16:47:47 +0000

    test bb output for create_aircraft cmd

--- PyDodo/tests/unit/test_create_aircraft.py
+++ PyDodo/tests/unit/test_create_aircraft.py
@@ -1,7 +1,11 @@
+"""
+Test create_aircraft
+"""
 
 import pytest
 import numpy as np
 from pydodo.create_aircraft import create_aircraft
+from pydodo.utils import ping_bluebird
 
 # aircraft ID and type stay the same
 aircraft_id = "TST1001"
@@ -83,3 +87,19 @@ def test_input_speed(speed):
     with pytest.raises(AssertionError):
         create_aircraft(aircraft_id, type, latitude, longitude,
                           heading, altitude, flight_level, speed)
+
+bb_resp = ping_bluebird()
+@pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
+def test_output_create_aircraft():
+    """
+    For valid inputs, check function returns True (if can connect to bluebird)
+    """
+    longitude = -180
+    latitude = -90
+    heading = 359.99
+    altitude = 0
+    flight_level = np.nan
+    speed = 0
+
+    output = create_aircraft(aircraft_id, type, latitude, longitude, heading, altitude, flight_level, speed)
+    assert output == True
