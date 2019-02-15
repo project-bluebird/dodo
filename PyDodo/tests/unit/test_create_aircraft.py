63fd30030257f775a3d2b010d99f0c1c2937d86f
radka-j <r.jersakova@gmail.com>
2019-02-15 16:20:19 +0000

    update test

--- PyDodo/tests/unit/test_create_aircraft.py
+++ PyDodo/tests/unit/test_create_aircraft.py
@@ -1,92 +1,24 @@
 """
-Test create_aircraft
+Test create_aircraft function raises error if incorrect inputs are provided.
+
+Function returns True if valid inputs are used and connection to bluebird exists.
 """
 
 import pytest
 import numpy as np
+
 from pydodo.create_aircraft import create_aircraft
 from pydodo.utils import ping_bluebird
 
-# aircraft ID and type stay the same
+# Valid input parameter values
 aircraft_id = "TST1001"
 type = "B744"
-
-@pytest.mark.parametrize("latitude", [91, -90.1])
-def test_input_latitude(latitude):
-    """
-    Check incorrect latitude inputs raise error
-    """
-    longitude = 0
-    heading = 0
-    altitude = np.nan
-    flight_level = "FL250"
-    speed = 200
-
-    with pytest.raises(AssertionError):
-        create_aircraft(aircraft_id, type, latitude, longitude,
-                          heading, altitude, flight_level, speed)
-
-@pytest.mark.parametrize("longitude", [180, -180.1])
-def test_input_longitude(longitude):
-    """
-    Check incorrect longitude inputs raise error
-    """
-    latitude = 0
-    heading = 0
-    altitude = np.nan
-    flight_level = "FL250"
-    speed = 200
-
-    with pytest.raises(AssertionError):
-        create_aircraft(aircraft_id, type, latitude, longitude,
-                          heading, altitude, flight_level, speed)
-
-@pytest.mark.parametrize("heading", [-0.1, 360])
-def test_input_heading(heading):
-    """
-    Check incorrect heading inputs raise error
-    """
-    longitude = 0
-    latitude = 0
-    altitude = np.nan
-    flight_level = "FL250"
-    speed = 200
-
-    with pytest.raises(AssertionError):
-        create_aircraft(aircraft_id, type, latitude, longitude,
-                          heading, altitude, flight_level, speed)
-
-@pytest.mark.parametrize(
-    "altitude,flight_level",
-    [(6000,"FL250"), (6001, np.nan), (np.nan, "FL59")]
-    )
-def test_input_alt_fl(altitude, flight_level):
-    """
-    Check incorrect combination of altitude and flight level inputs raise error
-    """
-    longitude = 0
-    latitude = 0
-    heading = 0
-    speed = 200
-
-    with pytest.raises(AssertionError):
-        create_aircraft(aircraft_id, type, latitude, longitude,
-                          heading, altitude, flight_level, speed)
-
-@pytest.mark.parametrize("speed", [-0.1])
-def test_input_speed(speed):
-    """
-    Check incorrect speed inputs raise error
-    """
-    longitude = 0
-    latitude = 0
-    heading = 0
-    altitude = np.nan
-    flight_level = "FL250"
-
-    with pytest.raises(AssertionError):
-        create_aircraft(aircraft_id, type, latitude, longitude,
-                          heading, altitude, flight_level, speed)
+latitude = 0
+longitude = 0
+heading = 0
+altitude = np.nan
+flight_level = 250
+speed = 200
 
 bb_resp = ping_bluebird()
 @pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
@@ -94,12 +26,66 @@ def test_output_create_aircraft():
     """
     For valid inputs, check function returns True (if can connect to bluebird)
     """
-    longitude = -180
-    latitude = -90
-    heading = 359.99
-    altitude = 0
-    flight_level = np.nan
-    speed = 0
-
-    output = create_aircraft(aircraft_id, type, latitude, longitude, heading, altitude, flight_level, speed)
+    output = create_aircraft(aircraft_id, type, latitude, longitude,
+                             heading, altitude, flight_level, speed)
     assert output == True
+
+@pytest.mark.parametrize(
+    "acid,tp",
+    [("", "B744"), ("TST1001", "")]
+)
+def test_input_aircraft_info(acid, tp):
+    """
+    Check incorrect aircraft_id and type inputs raise error
+    """
+    with pytest.raises(AssertionError):
+        create_aircraft(acid, tp, latitude, longitude,
+                        heading, altitude, flight_level, speed)
+
+@pytest.mark.parametrize("lat", [91, -90.1])
+def test_input_latitude(lat):
+    """
+    Check incorrect latitude inputs raise error
+    """
+    with pytest.raises(AssertionError):
+        create_aircraft(aircraft_id, type, lat, longitude,
+                        heading, altitude, flight_level, speed)
+
+@pytest.mark.parametrize("lon", [180, -180.1])
+def test_input_longitude(lon):
+    """
+    Check incorrect longitude inputs raise error
+    """
+    with pytest.raises(AssertionError):
+        create_aircraft(aircraft_id, type, latitude, lon,
+                        heading, altitude, flight_level, speed)
+
+@pytest.mark.parametrize("hdg", [-0.1, 360])
+def test_input_heading(hdg):
+    """
+    Check incorrect heading inputs raise error
+    """
+    with pytest.raises(AssertionError):
+        create_aircraft(aircraft_id, type, latitude, longitude,
+                        hdg, altitude, flight_level, speed)
+
+@pytest.mark.parametrize(
+    "alt,fl",
+    [(6000,250), (6001, np.nan), (np.nan, 59)]
+    )
+def test_input_alt_fl(alt, fl):
+    """
+    Check incorrect combination of altitude and flight level inputs raise error
+    """
+    with pytest.raises(AssertionError):
+        create_aircraft(aircraft_id, type, latitude, longitude,
+                        heading, alt, fl, speed)
+
+@pytest.mark.parametrize("spd", [-0.1])
+def test_input_speed(spd):
+    """
+    Check incorrect speed inputs raise error
+    """
+    with pytest.raises(AssertionError):
+        create_aircraft(aircraft_id, type, latitude, longitude,
+                        heading, altitude, flight_level, spd)
