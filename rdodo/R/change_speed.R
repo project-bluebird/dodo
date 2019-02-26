3047729ba5421860d2e893cb463d9cf04b3854af
Tim Hobson <thobson@turing.ac.uk>
2019-02-26 23:15:31 +0000

    Add change_speed function

---
+++ rdodo/R/change_speed.R
@@ -0,0 +1,31 @@
+#' Send a "change speed" command
+#'
+#' Request an aircraft to change speed.
+#'
+#' @param aircraft_id
+#' A string aircraft identifier
+#' @param speed
+#' A non-negative double. The aircraft's new speed in knots.
+#'
+#' @return
+#' \code{TRUE} if successful. Otherwise \code{FALSE} and an error is thrown.
+#'
+#' @examples
+#' \dontrun{
+#' change_speed("test1234", speed = 90)
+#' }
+#' @import config httr
+#' @export
+change_speed <- function(aircraft_id, speed) {
+
+  stopifnot(is.character(aircraft_id), length(aircraft_id) == 1)
+
+  # TODO: move to validate_speed function.
+  stopifnot(is.double(speed), length(speed) == 1, speed >= 0)
+
+  body <- list(
+    "acid" = aircraft_id,
+    "spd" = speed
+  )
+  post_call(endpoint = config::get("endpoint_change_speed"), body = body)
+}
