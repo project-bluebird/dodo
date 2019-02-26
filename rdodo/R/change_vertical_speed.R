3acfa4a3953e6ba57d4f5fded751216e8e2d3810
Tim Hobson <thobson@turing.ac.uk>
2019-02-26 23:27:59 +0000

    Add change_vertical_speed function

---
+++ rdodo/R/change_vertical_speed.R
@@ -0,0 +1,32 @@
+#' Send a "change vertical speed" command
+#'
+#' Request an aircraft to change vertical speed.
+#'
+#' @param aircraft_id
+#' A string aircraft identifier
+#' @param vertical_speed
+#' A non-negative double. The aircraft's new vertical speed in feet/min.
+#'
+#' @return
+#' \code{TRUE} if successful. Otherwise \code{FALSE} and an error is thrown.
+#'
+#' @examples
+#' \dontrun{
+#' change_vertical_speed("test1234", speed = 8)
+#' }
+#' @import config httr
+#' @export
+change_vertical_speed <- function(aircraft_id, vertical_speed) {
+
+  stopifnot(is.character(aircraft_id), length(aircraft_id) == 1)
+
+  # TODO: move to validate_vertical_speed function.
+  stopifnot(is.double(vertical_speed), length(vertical_speed) == 1,
+            vertical_speed >= 0)
+
+  body <- list(
+    "acid" = aircraft_id,
+    "vspd" = speed
+  )
+  post_call(endpoint = config::get("endpoint_change_vertical_speed"), body = body)
+}
---
+++ rdodo/tests/testthat/test-change_vertical_speed.R
@@ -0,0 +1,51 @@
+require(testthat)
+context("change_vertical_speed function")
+
+skip_if_not(found_bluebird(), message = "BlueBird not found: tests skipped.")
+
+# Reset the simulation to ensure no aircraft exist initially.
+reset_simulation()
+
+test_that("the change_speed function works", {
+
+  aircraft_id <- "test-change-vertical-speed"
+  type <- "B744"
+  latitude <- 0
+  longitude <- 0
+  heading <- 0
+  flight_level <- 250
+  speed <- 265
+
+  # Create an aircraft.
+  expect_true(create_aircraft(aircraft_id = aircraft_id,
+                              type = type,
+                              latitude = latitude,
+                              longitude = longitude,
+                              heading = heading,
+                              flight_level = flight_level,
+                              speed = speed))
+
+  # TODO: replace hard-coded string "vertical_speed" with config parameter (see also
+  # other list elements in aircraft_position):
+
+  # Check the vertical speed.
+  position <- aircraft_position(aircraft_id)
+  expect_identical(position[aircraft_id, "vertical_speed"], expected = 0)
+
+  # Give the command to ascend.
+  new_flight_level <- 450
+  expect_true(change_altitude(aircraft_id = aircraft_id,
+                              flight_level = new_flight_level))
+
+  # Test with an invalid vertical speed.
+  invalid_vertical_speed <- -1
+  expect_error(change_vertical_speed(aircraft_id = aircraft_id,
+                            vertical_speed = invalid_vertical_speed))
+
+  # Give the command to change vertical speed.
+  new_vertical_speed <- 10
+  expect_true(change_vertical_speed(aircraft_id = aircraft_id,
+                                    vertical_speed = new_vertical_speed))
+
+  # TODO: Check that the vertical speed has changed.
+})
