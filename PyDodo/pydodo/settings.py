46056204c927e330ce5605c46ffcd14764f79a42
radka-j <r.jersakova@gmail.com>
2019-02-28 17:23:44 +0000

    update config search in settings.py

--- PyDodo/pydodo/settings.py
+++ PyDodo/pydodo/settings.py
@@ -9,8 +9,21 @@ import yaml
 API_VERSION = 1
 
 this_dir, this_filename = os.path.split(os.path.abspath(__file__))
-with open(os.path.join(this_dir, "config.yml"), 'r') as ymlfile:
-    cfg = yaml.load(ymlfile)
+try:
+    """used for installs"""
+    with open(os.path.join(this_dir, "config.yml"), 'r') as ymlfile:
+        cfg = yaml.load(ymlfile)
+except:
+    """used for development"""
+    try:
+        with open("../config.yml", 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+    except:
+        try:
+            with open("../../config.yml", 'r') as ymlfile:
+                cfg = yaml.load(ymlfile)
+        except:
+            raise FileNotFoundError("The config file is missing.")
 
 default = cfg['default']
 BB_HOST = cfg['default']['host']
