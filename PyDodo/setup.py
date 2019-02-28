860f47916dba48aff6fb4723e6cc907da0fc2235
radka-j <r.jersakova@gmail.com>
2019-02-28 10:45:55 +0000

    add download config to setup

--- PyDodo/setup.py
+++ PyDodo/setup.py
@@ -1,7 +1,18 @@
+import os
 from setuptools import setup, find_packages
+from setuptools.command.install import _install
+from subprocess import call
+
+# BASEPATH = os.path.dirname(os.path.abspath(__file__))
 
 with open("requirements.txt", "r") as f:
     REQUIRED_PACKAGES = f.read().splitlines()
+
+class install(_install):
+    def run(self):
+        _install.run(self)
+        print("Getting the config file")
+        call(['wget', 'https://raw.githubusercontent.com/alan-turing-institute/dodo/master/config.yml'])
 
 setup(
     name = "PyDodo",
@@ -11,5 +22,6 @@ setup(
     install_requires=REQUIRED_PACKAGES,
     packages = ["pydodo"],
     # packages = find_packages(exclude=['*test']),
-    url="https://github.com/alan-turing-institute/dodo/PyDoDo"
+    url="https://github.com/alan-turing-institute/dodo/PyDoDo",
+    cmdclass={"install": install}
 )
