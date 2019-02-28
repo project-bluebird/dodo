f1c3110fba7ff9d4c00ab36584e0636ee4495233
Tim Hobson <thobson@turing.ac.uk>
2019-02-28 11:31:43 +0000

    Add onLoad function

---
+++ rdodo/R/onLoad.R
@@ -0,0 +1,39 @@
+# Runs whenever the package is loaded.
+#
+# Checks that a config.yml file exists and, if not, attempts to download one
+# from the dodo repository.
+#
+.onLoad <- function(libname, pkgname) {
+
+  # Check the config.yml file exists.
+  config_filename <- "config.yml"
+
+  config_file <- NA
+
+  if (file.exists(file.path("..", config_filename)))
+    config_file <- file.path("..", config_filename)
+  if (file.exists(config_filename))
+    config_file <- config_filename
+
+  # If necessary, pick up the config file from the dodo repository. Note there
+  # is no danger of overwriting an existing file, given the above.
+  if (is.na(config_file)) {
+    message("No config.yml file found. Attempting to download one...")
+
+    url <- "https://raw.githubusercontent.com/alan-turing-institute/dodo/master/config.yml"
+    response <- tryCatch({
+      utils::download.file(url, destfile = file.path(getwd(), config_filename))
+    },
+    error=function(cond) {
+      message("Error downloading rdodo config.yml file.")
+      stop(paste(conditionMessage(cond)))
+    })
+
+    if (response != 0L || !file.exists(config_filename))
+      warning(paste("Failed to download rdodo config.yml file. You will need to obtain one, e.g. from:\n", url))
+    message(paste("Downloaded rdodo configuration file:", config_filename))
+
+    config_file <- config_filename
+  }
+  message(paste("Using configuration file:", config_file))
+}
