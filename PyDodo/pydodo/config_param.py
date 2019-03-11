ada8681d7e3e5ad39c7191fdc3b6ac2f5b142df6
radka-j <r.jersakova@gmail.com>
2019-03-11 15:44:12 +0000

    rename settings.py to config_param.py, add find_config()

---
+++ PyDodo/pydodo/config_param.py
@@ -0,0 +1,29 @@
+
+import os
+import yaml
+
+def find_config():
+    """
+    Find and parse the config.yml file.
+    """
+    try:
+        """used for installs"""
+        this_dir, this_filename = os.path.split(os.path.abspath(__file__))
+        with open(os.path.join(this_dir, "config.yml"), 'r') as ymlfile:
+            cfg = yaml.load(ymlfile)
+    except:
+        """used for development"""
+        try:
+            with open("../config.yml", 'r') as ymlfile:
+                cfg = yaml.load(ymlfile)
+        except:
+            try:
+                with open("../../config.yml", 'r') as ymlfile:
+                    cfg = yaml.load(ymlfile)
+            except:
+                raise FileNotFoundError("The config file is missing.")
+    return cfg
+
+def config_param(param, config='default'):
+    cfg_file = find_config()
+    return cfg_file[config][param]
