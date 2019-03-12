66aef86bd29fe432486d5de98901e882e967037b
radka-j <r.jersakova@gmail.com>
2019-03-12 14:11:46 +0000

    add test for pause/resume functions

--- PyDodo/tests/integration/test_manage_simulation.py
+++ PyDodo/tests/integration/test_manage_simulation.py
@@ -5,12 +5,15 @@ Test load_scenario and reset_simulation functions
 """
 
 import pytest
-from pydodo import reset_simulation, load_scenario, pause_simulation, resume_simulation
+from pydodo import (create_aircraft, reset_simulation, load_scenario,
+                    pause_simulation, resume_simulation, aircraft_position)
 from pydodo.utils import ping_bluebird
 
 from requests.exceptions import HTTPError
 
 bb_resp = ping_bluebird()
+
+
 
 @pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
 def test_simulation():
@@ -20,18 +23,48 @@ def test_simulation():
     - Resume
     - Reset
     """
-    resp = load_scenario("scenario/8.scn")
-    assert resp == True
-
-    resp = pause_simulation()
-    assert resp == True
-
-    resp = resume_simulation()
-    assert resp == True
+    aircraft_id = "TST1001"
+    type = "B744"
+    latitude = 0
+    longitude = 0
+    heading = 0
+    altitude = None
+    flight_level = 250
+    speed = 200
 
     resp = reset_simulation()
     assert resp == True
 
+    resp = create_aircraft(aircraft_id, type, latitude, longitude,
+                             heading, speed, altitude, flight_level)
+    assert resp == True
+
+    pos0 = aircraft_position(aircraft_id)
+
+    resp = pause_simulation()
+    assert resp == True
+
+    pos1 = aircraft_position(aircraft_id)
+    # check that position has changed since last position call
+    assert pos1.loc[aircraft_id]["latitude"] > pos0.loc[aircraft_id]["latitude"]
+
+    pos2 = aircraft_position(aircraft_id)
+    # check that position has not changed since simulation was paused
+    assert pos1.loc[aircraft_id]["latitude"] == pos2.loc[aircraft_id]["latitude"]
+
+    resp = resume_simulation()
+    assert resp == True
+
+    pos3 = aircraft_position(aircraft_id)
+    # check that position has changed since simulation was resumed
+    assert pos3.loc[aircraft_id]["latitude"] > pos2.loc[aircraft_id]["latitude"]
+
+@pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
+def test_load_scenario():
+    resp = load_scenario("scenario/8.scn")
+    assert resp == True
+
+@pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
 def test_load_empty():
     """
     Check fails if no scenario file is provided
