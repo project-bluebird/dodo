bb3ae744d31a67e3d15267acf0c4d4819cecaad6
Tim Hobson <thobson@turing.ac.uk>
2019-06-05 15:00:59 +0100

    Add create_scenario function and sample scenario file

---
+++ rdodo/R/create_scenario.R
@@ -0,0 +1,30 @@
+#' Create a scenario
+#'
+#' @param filename
+#' The path to a scenario file on the local machine.
+#' @param scenario
+#' The name to store scenario under on the simulator host.
+#' @return
+#' \code{TRUE} if successful. Otherwise \code{FALSE} and an error is thrown.
+#'
+#' @examples
+#' \dontrun{
+#' create_scenario(system.file("dodo-test-scenario.scn", package = "rdodo"),
+#' scenario = "dodo-test-scenario")
+#' }
+#'
+#' @export
+create_scenario <- function(filename, scenario) {
+
+  stopifnot(is.character(filename), length(filename) == 1)
+  stopifnot(is.character(scenario), length(scenario) == 1)
+
+  # TODO: replace string literals with config parameters from Bluebird.
+  body <- list("scn_name" = scenario)
+
+  # Read lines from the file & add to the "content" element of the request body.
+  conn <- file(filename, open="r")
+  body <- c(body, list("content" = readLines(conn)))
+  close(conn)
+  post_call(endpoint = config_param("endpoint_create_scenario"), body = body)
+}
