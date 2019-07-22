6f526ed7cefa650927093b0d9aa2df395111ddc6
Tim Hobson <thobson@turing.ac.uk>
2019-07-22 13:54:23 +0100

    Add geodesic_separation function

--- rdodo/R/geodesic_separation.R
+++ rdodo/R/geodesic_separation.R
@@ -19,12 +19,21 @@
 geodesic_separation <- function(from_aircraft_id,
                                 to_aircraft_id = from_aircraft_id) {
 
-  #TODO:
-  # - Get aircraft positions
-  # - Call geodesic_distance with mapply to obtain a matrix
-  # - Add colnames/rownames
-  # - Convert to data frame
+  # Get aircraft positions.
+  pos <- aircraft_position(unique(c(from_aircraft_id, to_aircraft_id)))
+  lat <- config_param("latitude"); lon <- config_param("longitude")
 
+  # Construct a matrix to contain the results.
+  m <- matrix(NA, nrow = length(from_aircraft_id), ncol = length(to_aircraft_id),
+                dimnames = list(from_aircraft_id, to_aircraft_id))
 
-  0
+  # Fill the matrix.
+  sink <- sapply(from_aircraft_id, FUN = function(i) {
+    sapply(to_aircraft_id, FUN = function(j) {
+      m[i, j] <<- geodesic_distance(from_lat = pos[i, lat], from_lon = pos[i, lon],
+                        to_lat = pos[j, lat], to_lon = pos[j, lon])
+    })
+  })
+
+  as.data.frame(m)
 }
