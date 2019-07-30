00eda12a95aa70d8dc1b7443e16942421c05361f
Timothy Hobson <thobson@turing.ac.uk>
2019-07-30 11:52:16 +0100

    Add simulation_time function

---
+++ rdodo/R/simulation_time.R
@@ -0,0 +1,37 @@
+#' Get the current simulation time
+#'
+#' @return
+#' A \code{POSIXct} date-time object, if successful. Otherwise an error is thrown.
+#'
+#' @examples
+#' \dontrun{
+#' simulation_time()
+#' }
+#'
+#' @import httr
+#' @importFrom jsonlite fromJSON
+#' @export
+simulation_time <- function() {
+
+  endpoint <- config_param("endpoint_simulation_time")
+  url <- construct_endpoint_url(endpoint = endpoint)
+  response <- tryCatch({
+    httr::GET(url)
+  },
+  error=function(cond) {
+    stop(paste(conditionMessage(cond)))
+  })
+
+  validate_response(response)
+
+  l <- jsonlite::fromJSON(httr::content(response, "text"), simplifyVector = FALSE)
+
+  stopifnot(is.list(l), length(l) == 1)
+
+  # Identify the timezone.
+  name <- "sim_utc"
+  stopifnot(identical(names(l), name))
+  tz <- toupper(substring(name, first = nchar(name) - 2, last = nchar(name)))
+
+  as.POSIXct(l[[name]], tz = tz)
+}
