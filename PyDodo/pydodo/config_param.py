82acbfc72c9f30bfa535defba947c9844cd2b652
Radka Jersakova <r.jersakova@gmail.com>
2019-08-02 13:06:17 +0100

    add yaml_load to solve travis yaml issues

--- PyDodo/pydodo/config_param.py
+++ PyDodo/pydodo/config_param.py
@@ -2,6 +2,17 @@ import os
 import yaml
 
 
+def yaml_load(ymlfile):
+    """
+    Load yaml file. If PyYaml version < 5, first line won't work.
+    """
+    try:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    except:
+        cfg = yaml.load(ymlfile)
+    return cfg
+
+
 def find_config():
     """
     Find and parse the config.yml file.
@@ -10,16 +21,19 @@ def find_config():
         """used for installs"""
         this_dir, this_filename = os.path.split(os.path.abspath(__file__))
         with open(os.path.join(this_dir, "config.yml"), "r") as ymlfile:
-            cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+            #cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+            cfg = yaml_load(ymlfile)
     except:
         """used for development"""
         try:
             with open("../config.yml", "r") as ymlfile:
-                cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+                # cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+                cfg = yaml_load(ymlfile)
         except:
             try:
                 with open("../../config.yml", "r") as ymlfile:
-                    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+                    # cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+                    cfg = yaml.load(ymlfile)
             except:
                 raise FileNotFoundError("The config file is missing.")
     return cfg
