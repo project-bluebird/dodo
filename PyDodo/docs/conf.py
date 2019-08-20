997cdd78fa216264ee4628b1d979f740675582a0
Tarek Allam <t.allam.jr@gmail.com>
2019-08-20 17:53:29 +0100

    Update Author name

--- PyDodo/docs/conf.py
+++ PyDodo/docs/conf.py
@@ -60,8 +60,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pydodo'
-copyright = '2019, Alan Turing'
-author = 'Alan Turing'
+copyright = '2019, Alan Turing Institute'
+author = 'Alan Turing Institute'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
