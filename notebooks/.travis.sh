b8a0872457e2967eb5ede1285f42e76b3c652e51
Tarek Allam <t.allam.jr@gmail.com>
2019-08-29 13:30:09 +0100

    Updating CI script to test example notebooks

---
+++ notebooks/.travis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -ev
+if [ $TRAVIS_OS_NAME = "linux" ]; then
+    sudo apt-get update
+    # Get set up to run jupyter notebooks:
+    sudo apt-get install texlive-latex-recommended
+    sudo apt-get install texlive-latex-extra
+    sudo apt-get install texlive-fonts-recommended
+    sudo apt-get install chktex
+    sudo apt-get install dvipng
+    sudo apt-get install pandoc
+    pip install --upgrade jupyter
+    pip install nbconvert
+    jupyter nbconvert --ExecutePreprocessor.kernel_name=python --ExecutePreprocessor.timeout=600 --to html --execute notebooks/Python-example-notebook.ipynb
+    jupyter nbconvert --ExecutePreprocessor.kernel_name=r --ExecutePreprocessor.timeout=600 --to html --execute notebooks/R-example-notebook.ipynb
+fi
