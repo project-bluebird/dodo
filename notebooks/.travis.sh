d99916d242f18c07d10aee8908533b136bfc1f6c
Tarek Allam <t.allam.jr@gmail.com>
2019-09-10 22:31:52 +0100

    Fix install error
    Error:
        Install tornado itself to use zmq with the tornado IOLoop.
    
    REF:
        https://stackoverflow.com/a/56484803/4521950
    	modified:   notebooks/.travis.sh

--- notebooks/.travis.sh
+++ notebooks/.travis.sh
@@ -12,6 +12,10 @@ if [ $TRAVIS_OS_NAME = "linux" ]; then
     pip install --upgrade jupyter
     pip install nbconvert
     pip install -r PyDodo/requirements.txt
+    pip install --upgrade --force-reinstall pyzmq
+    pip install PyDodo/
+    docker-compose up -d
     jupyter nbconvert --ExecutePreprocessor.kernel_name=python --ExecutePreprocessor.timeout=600 --to html --execute notebooks/Python-example-notebook.ipynb
     jupyter nbconvert --ExecutePreprocessor.kernel_name=ir --ExecutePreprocessor.timeout=600 --to html --execute notebooks/R-example-notebook.ipynb
+    docker-compose down
 fi
