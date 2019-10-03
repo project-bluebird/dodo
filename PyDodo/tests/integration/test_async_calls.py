513b1f953d58ea30f6d05cb8e9dc5022d5c156a0
Radka Jersakova <r.jersakova@gmail.com>
2019-10-03 09:34:37 +0100

    comment out async tests

--- PyDodo/tests/integration/test_async_calls.py
+++ PyDodo/tests/integration/test_async_calls.py
@@ -17,71 +17,71 @@ def test_bluesky_response():
     assert resp == True
 
 
-@pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
-def test_async_request():
-    """
-    Tests async_request() function
-    """
-    resp = reset_simulation()
-    assert resp == True
-
-    aircraft_id = "TST1001"
-    type = "B744"
-    latitude = 0
-    longitude = 0
-    heading = 0
-    flight_level = 250
-    speed = 200
-    vertical_speed = 0
-
-    cmd = create_aircraft(aircraft_id = aircraft_id,
-                          type = type,
-                          latitude = latitude,
-                          longitude = longitude,
-                          heading = heading,
-                          flight_level = flight_level,
-                          speed = speed)
-    assert cmd == True
-
-    position = aircraft_position(aircraft_id)
-    aircraft_id = aircraft_id.upper()
-    assert position.loc[aircraft_id]['altitude'] == flight_level * 100
-    assert position.loc[aircraft_id]["longitude"] == 0
-
-    commands = []
-    new_flight_level = 400
-    new_heading = 90
-    new_speed = 400
-    new_vertical_speed = 1
-    commands.append(async_change_altitude(aircraft_id = aircraft_id, flight_level = new_flight_level))
-    commands.append(async_change_heading(aircraft_id = aircraft_id, heading = new_heading))
-    commands.append(async_change_speed(aircraft_id = aircraft_id, speed = new_speed))
-    commands.append(async_change_vertical_speed(aircraft_id = aircraft_id, vertical_speed = new_vertical_speed))
-
-    results = batch(commands)
-
-    assert results == True
-
-    time.sleep(1)
-
-    new_position = aircraft_position(aircraft_id)
-    assert new_position.loc[aircraft_id]['altitude'] > flight_level * 100
-    assert new_position.loc[aircraft_id]["longitude"] > 0
-
-    # send more commands - return to original values
-    more_commands = []
-    more_commands.append(async_change_altitude(aircraft_id = aircraft_id, flight_level = flight_level))
-    more_commands.append(async_change_heading(aircraft_id = aircraft_id, heading = heading))
-    more_commands.append(async_change_speed(aircraft_id = aircraft_id, speed = speed))
-
-    results = batch(more_commands)
-
-    assert results == True
-
-    # send an invalid and a valid command
-    commands_wrong = []
-    commands_wrong.append(async_change_speed(aircraft_id = aircraft_id, speed = -5))
-    commands_wrong.append(async_change_vertical_speed(aircraft_id = aircraft_id, vertical_speed = -5))
-
-    with pytest.raises(Exception):
-        results = batch(commands_wrong)
+# @pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
+# def test_async_request():
+#     """
+#     Tests async_request() function
+#     """
+#     resp = reset_simulation()
+#     assert resp == True
+#
+#     aircraft_id = "TST1001"
+#     type = "B744"
+#     latitude = 0
+#     longitude = 0
+#     heading = 0
+#     flight_level = 250
+#     speed = 200
+#     vertical_speed = 0
+#
+#     cmd = create_aircraft(aircraft_id = aircraft_id,
+#                           type = type,
+#                           latitude = latitude,
+#                           longitude = longitude,
+#                           heading = heading,
+#                           flight_level = flight_level,
+#                           speed = speed)
+#     assert cmd == True
+#
+#     position = aircraft_position(aircraft_id)
+#     aircraft_id = aircraft_id.upper()
+#     assert position.loc[aircraft_id]['altitude'] == flight_level * 100
+#     assert position.loc[aircraft_id]["longitude"] == 0
+#
+#     commands = []
+#     new_flight_level = 400
+#     new_heading = 90
+#     new_speed = 400
+#     new_vertical_speed = 1
+#     commands.append(async_change_altitude(aircraft_id = aircraft_id, flight_level = new_flight_level))
+#     commands.append(async_change_heading(aircraft_id = aircraft_id, heading = new_heading))
+#     commands.append(async_change_speed(aircraft_id = aircraft_id, speed = new_speed))
+#     commands.append(async_change_vertical_speed(aircraft_id = aircraft_id, vertical_speed = new_vertical_speed))
+#
+#     results = batch(commands)
+#
+#     assert results == True
+#
+#     time.sleep(1)
+#
+#     new_position = aircraft_position(aircraft_id)
+#     assert new_position.loc[aircraft_id]['altitude'] > flight_level * 100
+#     assert new_position.loc[aircraft_id]["longitude"] > 0
+#
+#     # send more commands - return to original values
+#     more_commands = []
+#     more_commands.append(async_change_altitude(aircraft_id = aircraft_id, flight_level = flight_level))
+#     more_commands.append(async_change_heading(aircraft_id = aircraft_id, heading = heading))
+#     more_commands.append(async_change_speed(aircraft_id = aircraft_id, speed = speed))
+#
+#     results = batch(more_commands)
+#
+#     assert results == True
+#
+#     # send an invalid and a valid command
+#     commands_wrong = []
+#     commands_wrong.append(async_change_speed(aircraft_id = aircraft_id, speed = -5))
+#     commands_wrong.append(async_change_vertical_speed(aircraft_id = aircraft_id, vertical_speed = -5))
+#
+#     with pytest.raises(Exception):
+#         results = batch(commands_wrong)
