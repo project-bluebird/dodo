c72f9d581f89d89818f2754c21d2e6d7823d7337
radka-j <r.jersakova@gmail.com>
2019-10-09 16:00:50 +0100

    refactor

--- PyDodo/pydodo/request_position.py
+++ PyDodo/pydodo/request_position.py
@@ -10,6 +10,10 @@ from . import utils
 endpoint = config_param("endpoint_aircraft_position")
 url = construct_endpoint_url(endpoint)
 
+_POS_COL_NAMES = ["type", "altitude", "ground_speed", "latitude", "longitude",
+"vertical_speed"]
+_SCALE_METRES_TO_FEET = 3.280839895
+
 
 def format_pos_info(aircraft_pos):
     """
@@ -35,7 +39,7 @@ def format_pos_info(aircraft_pos):
 
     Returns
     -------
-    position_formatted : dict
+    dict
         Dictionary of formatted aircraft position information with keys:
 
         ``"type"``
@@ -52,7 +56,7 @@ def format_pos_info(aircraft_pos):
             The aircraft's vertical speed in feet/min.
     """
 
-    position_formatted = {
+    return {
         "type": aircraft_pos["actype"],
         "altitude": aircraft_pos["alt"],
         "ground_speed": aircraft_pos["gs"],
@@ -60,7 +64,16 @@ def format_pos_info(aircraft_pos):
         "longitude": aircraft_pos["lon"],
         "vertical_speed": aircraft_pos["vs"],
     }
-    return position_formatted
+
+
+def normalise_positions_units(df):
+    """
+    Normalise units of measurement in the positions data.
+    """
+    # Bluesky returns altitude in metres, not feet.
+    if config_param("simulator") == config_param("bluesky_simulator"):
+        df.loc[:, "altitude"] = (_SCALE_METRES_TO_FEET * df["altitude"]).round(2)
+    return df
 
 
 def process_pos_response(response):
@@ -97,55 +110,67 @@ def process_pos_response(response):
     }
     pos_df = pd.DataFrame.from_dict(pos_dict, orient="index")
     pos_df.sim_t = json_data["sim_t"]
-    return pos_df
+    return normalise_positions_units(pos_df)
 
 
-def normalise_positions_units(df):
+def position_call(aircraft_id = None):
     """
-    Normalise units of measurement in the positions data.
-    """
-
-    SCALE_METRES_TO_FEET = 3.280839895
-
-    # Bluesky returns altitude in metres, not feet.
-    if config_param("simulator") == config_param("bluesky_simulator"):
-        df.loc[:, "altitude"] = SCALE_METRES_TO_FEET * df["altitude"]
-        df.loc[:, "altitude"] = df["altitude"].round(2)
-    return df
-
-
-def null_pos_df(aircraft_id=None):
-    """
-    Returns an empty dataframe if no aircraft_id is provided otherwise
-    dataframe with NANs.
+    Make a call to the BlueBird aircraft position (POS) endpoint.
 
     Parameters
     ----------
-    aircraft_id : str, optional
+    aircraft_id: str, optional
+        A string aircraft identifier, or None (the default).
 
     Returns
     -------
-    pos_df : pandas.DataFrame
+    pandas.DataFrame
+        Dataframe indexed by **uppercase** aircraft ID with columns:
+    | - ``type``: A string ICAO aircraft type designator.
+    | - ``altitude``: A non-negatige double. The aircraft's altitude in feet.
+    | - ``ground_speed``: A non-negative double. The aircraft's ground speed in knots.
+    | - ``latitude``: A double in the range ``[-90, 90]``. The aircraft's latitude.
+    | - ``longitude``: A double in the range ``[-180, 180]``. The aircraft's longitude.
+    | - ``vertical_speed``: A double. The aircraft's vertical speed in feet/min (units according to BlueSky docs).
+
+    Notes
+    -----
+    This dataframe also contains a metadata attribute named `sim_t` containing
+    the simulator time in seconds since the start of the scenario.
+
+    If no aircraft exists an empty data frame is returned.
+
+    If the given aircraft ID does not exist in the simulation, dataframe with a
+    row of missing values is returned.
+
+    If the response from Bluebird contains an error status code, an exception is
+    thrown.
+
+    Examples
+    ---------
+    >>> pydodo.request_position.position_call()
+    >>> pydodo.request_position.position_call("BAW123")
     """
 
-    null_dict = {
-        "type": [],
-        "altitude": [],
-        "ground_speed": [],
-        "latitude": [],
-        "longitude": [],
-        "vertical_speed": [],
-    }
     if aircraft_id == None:
-        return pd.DataFrame(null_dict)
+        aircraft_id = "all"
+    resp = requests.get(url, params={config_param("query_aircraft_id"): aircraft_id})
+    if resp.status_code == 200:
+        return process_pos_response(resp)
+    elif resp.status_code == config_param("status_code_aircraft_id_not_found"):
+        return pd.DataFrame(
+            {key: np.nan for key in _POS_COL_NAMES},
+            index=[aircraft_id]
+        )
+    elif resp.status_code == config_param("status_code_no_aircraft_found"):
+        return pd.DataFrame({col: [] for col in _POS_COL_NAMES})
     else:
-        nan_dict = {key: np.nan for key in null_dict.keys()}
-        return pd.DataFrame(nan_dict, index=[aircraft_id])
+        raise requests.HTTPError(resp.text)
 
 
 def all_positions():
     """
-    Get dataframe with position information for all aircraft in simulation.
+    Get all aircraft positions.
 
     Parameters
     ----------
@@ -177,56 +202,7 @@ def all_positions():
     >>> pydodo.all_positions()
     """
 
-    resp = requests.get(url, params={config_param("query_aircraft_id"): "all"})
-    if resp.status_code == 200:
-        pos_df = process_pos_response(resp)
-        return normalise_positions_units(pos_df)
-    elif resp.status_code == config_param("status_code_no_aircraft_found"):
-        return null_pos_df()
-    else:
-        raise requests.HTTPError(resp.text)
-
-
-def get_position(aircraft_id):
-    """
-    Get position dataframe for a single aircraft_id.
-
-    Parameters
-    ----------
-    aircraft_id : str
-        A string aircraft identifier. For the BlueSky simulator, this has to be
-        at least three characters.
-
-    Returns
-    -------
-    pos_df : pandas.DataFrame
-        Dataframe indexed by **uppercase** aircraft ID with columns:
-    | - ``type``: A string ICAO aircraft type designator.
-    | - ``altitude``: A non-negatige double. The aircraft's altitude in feet.
-    | - ``ground_speed``: A non-negative double. The aircraft's ground speed in knots.
-    | - ``latitude``: A double in the range ``[-90, 90]``. The aircraft's latitude.
-    | - ``longitude``: A double in the range ``[-180, 180)``. The aircraft's longitude.
-    | - ``vertical_speed``: A double. The aircraft's vertical speed in feet/min (units according to BlueSky docs).
-
-    Notes
-    -----
-    This dataframe also contains a metadata attribute named `sim_t` containing
-    the simulator time in seconds since the start of the scenario.
-
-    If the given aircraft ID does not exist in the simulation, the returned
-    dataframe is a row of missing values.
-
-    If an invalid ID is given, or the call to Bluebird fails, an exception is
-    thrown.
-    """
-
-    resp = requests.get(url, params={config_param("query_aircraft_id"): aircraft_id})
-    if resp.status_code == 200:
-        return process_pos_response(resp)
-    elif resp.status_code == config_param("status_code_aircraft_id_not_found"):
-        return null_pos_df(aircraft_id)
-    else:
-        raise requests.HTTPError(resp.text)
+    return position_call()
 
 
 def aircraft_position(aircraft_id):
@@ -268,12 +244,11 @@ def aircraft_position(aircraft_id):
 
     if type(aircraft_id) == str:
         utils._validate_id(aircraft_id)
-        pos_df = get_position(aircraft_id)
+        return position_call(aircraft_id)
     elif type(aircraft_id) == list and bool(aircraft_id):
         for aircraft in aircraft_id:
             utils._validate_id(aircraft)
-        all_pos = all_positions()
-        pos_df = all_pos.reindex(aircraft_id)  # filter requested IDs
+        all_pos = position_call() # get all aircraft in simulation
+        return all_pos.reindex(aircraft_id)  # filter requested IDs
     else:
         raise AssertionError("Invalid input {} for aircraft id".format(aircraft_id))
-    return normalise_positions_units(pos_df)
