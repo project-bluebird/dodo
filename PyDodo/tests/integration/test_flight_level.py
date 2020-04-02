87658740c8de04e80c590ee948ea744aa7d2f5d3
radka-j <r.jersakova@gmail.com>
2020-04-02 11:28:45 +0200

    add test for get FL functions

--- PyDodo/tests/integration/test_flight_level.py
+++ PyDodo/tests/integration/test_flight_level.py
@@ -1,6 +1,6 @@
 import pytest
 
-from pydodo import reset_simulation
+from pydodo import reset_simulation, all_positions
 from pydodo import current_flight_level, cleared_flight_level, requested_flight_level
 from pydodo.bluebird_connect import ping_bluebird
 
@@ -8,17 +8,23 @@ bb_resp = ping_bluebird()
 
 
 @pytest.mark.skipif(not bb_resp, reason="Can't connect to bluebird")
-def test_flight_level():
+def test_flight_level(upload_test_sector_scenario):
+
     cmd = reset_simulation()
     assert cmd == True
 
-    # resp = load_scenario("scenario/8.SCN")
-    # assert resp == True
-    #
-    # assert current_flight_level("SCN1001") == 6096
-    # assert cleared_flight_level("SCN1001") == 6096
-    # assert requested_flight_level("SCN1001") == None
-    #
-    # assert current_flight_level("SCN1005") == 2438
-    # assert cleared_flight_level("SCN1005") == 2438
-    # assert requested_flight_level("SCN1005") == None
+    upload_test_sector_scenario()
+
+    # Get the position
+    position = all_positions()
+    acid1, acid2 = position.index
+
+    assert cleared_flight_level(acid1) == 400
+    assert requested_flight_level(acid1) == 400
+
+    # In BlueSky overflier aircraft is inialised below requested flight level
+    assert 39995 < current_flight_level(acid1) <= 40000
+
+    assert cleared_flight_level(acid2) == 200
+    assert requested_flight_level(acid2) == 400
+    assert current_flight_level(acid2) == 20000
